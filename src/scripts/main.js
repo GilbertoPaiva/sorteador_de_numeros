d90e9ae672ac2fa5b5321ecedb10bf68a0330ca4
GilbertoPaiva <gilberto.melo.paiva@gmail.com>
2025-11-25 18:44:37 -0300

    Adiciona funcionalidade de easter egg

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(event) {
+    let sequencia = [];
+    const codigoSecreto = ['l', 'a', 'r', 'a'];
+    
+    document.addEventListener('keypress', function(e) {
+        sequencia.push(e.key.toLowerCase());
+        sequencia = sequencia.slice(-codigoSecreto.length);
+        
+        if (sequencia.join('') === codigoSecreto.join('')) {
+            document.getElementById('resultado-valor').innerText = 'Te amo princesinha 💚';
+            document.querySelector('.resultado').style.display = 'block';
+            sequencia = [];
+        }
+    });
+
     document.getElementById('form-sorteador').addEventListener('submit', function(event) {
         event.preventDefault();
         let numeroMaximo = document.getElementById('numero-maximo').value;
@@ -7,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
         let numeroAleatorio = Math.random() * numeroMaximo;
         numeroAleatorio = Math.floor(numeroAleatorio) + 1;
 
-        document.getElementById('resultado-valor').innerText = numeroAleatorio;
+        document.getElementById('resultado-valor').innerText = mensagem;
         document.querySelector('.resultado').style.display = 'block';
     })
 })
